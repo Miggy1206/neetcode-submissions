c8b7de15278911ac50fc3c22f64d076e49b7e63b
Miguel Santos Barbosa <150379635+Miggy1206@users.noreply.github.com>
2026-09-15 12:36:09 +0100

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        st = ""
+        for char in s:
+            if (char.isalnum()):
+                st+=char
+        return st.lower() == st[::-1].lower()
+        
