e4326ea5eb58a5033630dd630152392547002a04
Miguel Santos Barbosa <150379635+Miggy1206@users.noreply.github.com>
2026-09-15 12:34:34 +0100

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        st = ""
+        for char in s:
+            if (ord(char) >= 65 and ord(char) <= 90) or (97 <= ord(char) and ord(char) <= 122) or (ord(char) >= 48 and ord(char) <= 57 ):
+                st+=char
+        print(st.lower())
+        print(st[::-1].lower())
+        return st.lower() == st[::-1].lower()
+        
