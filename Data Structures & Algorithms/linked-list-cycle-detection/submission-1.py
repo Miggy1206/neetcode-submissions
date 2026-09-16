afee9f92ccfe1aa0e8ba515aef7151694f05d266
Miguel Santos Barbosa <150379635+Miggy1206@users.noreply.github.com>
2026-09-16 20:37:29 +0100

    Add: linked-list-cycle-detection - submission-1

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-1.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+# 1 2 3 4 2 3 4 2 3 4 2 3 4
+# 1 3 2 4
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+
+
+
+
+
+        return False
+        
